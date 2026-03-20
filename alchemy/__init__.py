0720d9985be587f00c5a2e0868d674675528c541
Yoru <swearwings@gmail.com>
2026-03-20 14:32:10 +0000

    i commented things

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -8,3 +8,11 @@ from .elements import create_fire, create_water
 # And this below me is...idk flake8 gets fussy
 # but basically this is like "what available stuff i have"
 __all__ = ["create_fire", "create_water"]
+
+
+"""
+why init.py? u might ask
+basically besides init being a constructor in OOP,
+init is also a "diy module maker" so you can import your
+custom things as well, so its like "hey! im a module too!"
+"""
